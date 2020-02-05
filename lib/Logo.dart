02a56e75a032785df4dffb827d203fe32b2c8d58
bulinwilliam <bulinwilliam>
2020-02-05 23:29:54 +0100

    Added new icon

--- lib/Logo.dart
+++ lib/Logo.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Logo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      "https://i.imgur.com/85UVbyR.png",
-      width: 350,
-      filterQuality: FilterQuality.high,
-      fit: BoxFit.contain,
+    // return SvgPicture.asset(
+    //   "assets/logo.svg",
+    //   semanticsLabel: 'Acme Logo',
+    //   width: 350,
+    // );
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Image.network(
+          "https://i.imgur.com/okPRaUc.png",
+          width: 100,
+          filterQuality: FilterQuality.high,
+          fit: BoxFit.contain,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Text(
+            "Alexandrio",
+            style: TextStyle(
+              fontSize: 48.0,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
